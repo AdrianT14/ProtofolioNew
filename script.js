4e49bffcf8410dacd24da1299fd687bc29da3968
Trifan Ioan Adrian <ionut.trifan98@gmail.com>
2022-03-26 20:27:21 +0200

    resposive

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+// NAV MENU
+
+const menu = document.querySelector(".nav-menu");
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+
+openMenu.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeMenu.style.display = "inline-block";
+  openMenu.style.display = "none";
+});
+
+closeMenu.addEventListener("click", () => {
+  menu.style.display = "none";
+  closeMenu.style.display = "none";
+  openMenu.style.display = "inline-block";
+});
+
+document.querySelectorAll(".menu-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    menu.style.display = "none";
+    closeMenu.style.display = "none";
+    openMenu.style.display = "inline-block";
+  })
+);
